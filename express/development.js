b0765204aa12d8e80f32c97bcf4096708809a57c
m.haghighakthah <haghighatkhah.mohsen@yahoo.com>
2020-08-31 23:47:57 +0430

    add express devlopment file

---
+++ express/development.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const webpack = require('webpack');
+const config = require('./../express/development.js');
+const compiler = webpack(config);
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const webpackHotServerMiddleware = require('webpack-hot-server-middleware');
+
+
+app.use(webpackDevMiddleware(compiler,{
+    serverSideRender: true,
+    publicPath: '/dist/'
+}));
+
+app.use(webpackHotMiddleware(compiler.compilers.find(compiler => compiler.name === 'client')));
+app.use(webpackHotServerMiddleware(comiler));
+
+
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT,error =>{
+    if(error)
+        return console.error(error);
+    else
+        console.log(`Development Express server running at http://localhost:${PORT}`);
+})
