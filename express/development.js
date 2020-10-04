23f50aa94630a6fdbd92db0f620e0fa72c56370c
m.haghighakthah <haghighatkhah.mohsen@yahoo.com>
2020-10-04 16:01:36 +0330

    Change some config on webpack

--- express/development.js
+++ express/development.js
@@ -1,28 +1,28 @@
 const express = require('express');
 const app = express();
 const webpack = require('webpack');
-const config = require('./../express/development.js');
+const config = require('./../webpack/development');
 const compiler = webpack(config);
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 const webpackHotServerMiddleware = require('webpack-hot-server-middleware');
 
-
-app.use(webpackDevMiddleware(compiler,{
+app.use(webpackDevMiddleware(compiler, {
     serverSideRender: true,
-    publicPath: '/dist/'
+    publicPath: "/dist/",
 }));
-
 app.use(webpackHotMiddleware(compiler.compilers.find(compiler => compiler.name === 'client')));
-app.use(webpackHotServerMiddleware(comiler));
-
+app.use(webpackHotServerMiddleware(compiler));
 
 const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, error => {
+    if (error) {
 
-app.listen(PORT,error =>{
-    if(error)
         return console.error(error);
-    else
+
+    } else {
+
         console.log(`Development Express server running at http://localhost:${PORT}`);
-})
+    }
+});
